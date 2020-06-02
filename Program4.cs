0746e906f2afb54145efd9ee429235c51e3517f1
Brian Weeteling <brianweeteling@gmail.com>
2020-06-03 01:16:21 +0200

    Task 4: binary search (96-128ms)

--- Program4.cs
+++ Program4.cs
@@ -9,10 +9,19 @@ namespace LeetCode
         {
             var solution = new Solution4();
 
+            // 1,3,7,8,9,11,15,18,19,21,25 -> 11
+            Run(solution, new[] { 1,3,8,9,15 }, new[] { 7, 11, 18, 19, 21, 25 }, 11.0);
+            // 1,3,7,8,9,11,15,18,19,21,25 -> 11
+            Run(solution, new[] { 23,26,31,35 }, new[] { 3,5,7,9,11,16 }, 13.5);
+
             // 1,2,3 -> 2
             Run(solution, new[] { 1, 3 }, new[] { 2 }, 2.0);
             // 1,2,3,4 -> 2.5
             Run(solution, new[] { 1, 2 }, new[] { 3, 4 }, 2.5);
+            // 1,2,3 -> 2
+            Run(solution, new[] { 1, 2, 3 }, new int[0], 2.0);
+            // 1,2,3 -> 2
+            Run(solution, new int[0], new int[] { 1, 2, 3 }, 2.0);
 
             static void Run(Solution4 solution, int[] nums1, int[] nums2, double expected)
             {
@@ -23,13 +32,45 @@ namespace LeetCode
     }
     public class Solution4
     {
-        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        public double FindMedianSortedArrays(int[] x, int[] y)
         {
-            var sorted = nums1.Concat(nums2).OrderBy(x => x);
-            var total = (nums1.Length + nums2.Length);
-            var middle = total / 2;
-            return total % 2 == 1 ?
-                sorted.ElementAt(middle) : (sorted.ElementAt(middle - 1) + sorted.ElementAt(middle)) / 2.0;
+            if (x.Length > y.Length)
+            {
+                return FindMedianSortedArrays(y, x);
+            }
+            var start = 0;
+            var end = x.Length;
+            while (start <= end)
+            {
+                var splitX = (start + end) / 2;
+                var splitY = ((x.Length + y.Length + 1) / 2) - splitX;
+
+                var maxLeftX = splitX == 0 ? int.MinValue : x[splitX - 1];
+                var minRightX = splitX == x.Length ? int.MaxValue : x[splitX];
+                var maxLeftY = splitY == 0 ? int.MinValue : y[splitY - 1];
+                var minRightY = splitY == y.Length ? int.MaxValue : y[splitY];
+
+                if (maxLeftX <= minRightY && maxLeftY <= minRightX)
+                {
+                    if ((x.Length + y.Length) % 2 == 0)
+                    {
+                        return (Math.Max(maxLeftX, maxLeftY) + Math.Min(minRightX, minRightY)) / 2.0;
+                    }
+                    else
+                    {
+                        return Math.Max(maxLeftX, maxLeftY);
+                    }
+                }
+                else if (maxLeftX > minRightY)
+                {
+                    end = splitX - 1;
+                }
+                else
+                {
+                    start = splitX + 1;
+                }
+            }
+            throw new Exception();
         }
     }
 }
